747d0287235dbe81ea006fc568cd9a07821a4337
z <z@bogon.TendaAP>
2015-08-08 23:29:19 +0800

    add view-注意导入顺序

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,18 @@
 from flask import Flask
-from .material.views import material
-from .bom.views import bom
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
 # 加载配置文件
 app.config.from_object('config')
 
 # 数据库
 db = SQLAlchemy(app)
 
+# 待db初始化后再引用views,因为views会引用models，而models会引用app里的db（就是这里的db）。如果把from .bom.views import bom和from .material.views import material放在db = SQLAlchemy(app)的前面，就会出现“ImportError: cannot import name 'db'”的错误。
 # 蓝图
+from .bom.views import bom
+from .material.views import material
 app.register_blueprint(bom, url_prefix='/bom')
 app.register_blueprint(material, url_prefix='/material')
 
--- app/material/views.py
+++ app/material/views.py
@@ -1,7 +1,18 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from .models import Material
 
 material = Blueprint('material', __name__)
 
 @material.route('/')
 def hello():
     return 'Hello material!'
+
+@material.route('/materials')
+def list():
+    materials = Material.query.all()
+    res = {}
+    for m in materials:
+        res[m.id] = {
+            'name': m.name
+        }
+    return jsonify(res)
