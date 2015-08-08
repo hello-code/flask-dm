f4603a2eb4058074b853f723d368ce91e3c6242c
z <z@bogon.TendaAP>
2015-08-08 18:36:38 +0800

    add model- 添加模型

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,18 @@
 from flask import Flask
 from .material.views import material
 from .bom.views import bom
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 # 加载配置文件
 app.config.from_object('config')
 
+# 数据库
+db = SQLAlchemy(app)
+
 # 蓝图
 app.register_blueprint(bom, url_prefix='/bom')
 app.register_blueprint(material, url_prefix='/material')
+
+# 创建数据库
+db.create_all()
