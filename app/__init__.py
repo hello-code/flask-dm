af824360ec2a1ea44b4f731d0fe3a2f136acc5fa
z <z@bogon.TendaAP>
2015-08-08 18:24:34 +0800

    写配置文件

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,9 @@ from .material.views import material
 from .bom.views import bom
 
 app = Flask(__name__)
+# 加载配置文件
+app.config.from_object('config')
+
+# 蓝图
 app.register_blueprint(bom, url_prefix='/bom')
 app.register_blueprint(material, url_prefix='/material')
