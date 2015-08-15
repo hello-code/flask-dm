8bfd3862bc714fd1a54cc1576cb0b31798541183
z <z@bogon.TendaAP>
2015-08-15 21:05:04 +0800

    sqlite文件的正确路径

--- app/material/views.py
+++ app/material/views.py
@@ -16,3 +16,17 @@ def list():
             'name': m.name
         }
     return jsonify(res)
+
+from sqlite3 import connect
+import config
+
+@material.route('/add')
+def add():
+    # conn = connect(app.config["SQLALCHEMY_DATABASE_URI "])  # wrong!
+    # 数据库文件路径:sqlite://///home/z/projects/flask/flask-dm/mydb.db
+    db_path = config.SQLALCHEMY_DATABASE_URI
+    conn = connect(db_path[11:])  # /home/z/projects/flask/flask-dm/mydb.db
+    curs = conn.cursor()
+    curs.execute('insert into material values(?,?)', (2, 'm2'))
+    conn.commit()
+    return str(curs.rowcount)
--- config.py
+++ config.py
@@ -1,3 +1,7 @@
+import os
+
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+SITE_PATH = os.path.dirname(os.path.abspath(__file__))  # flask-dm
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(SITE_PATH, "mydb.db")
 SECRET_KEY = 'DEV KEY'
