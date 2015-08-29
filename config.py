3369cc32045fe7cef826a856860610c866a84110
z <z@bogon.TendaAP>
2015-08-29 13:20:08 +0800

    boostrap use local js and css file

--- config.py
+++ config.py
@@ -7,3 +7,4 @@ SITE_PATH = os.path.dirname(os.path.abspath(__file__))  # flask-dm
 # SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.join(SITE_PATH, "mydb.db")
 SQLALCHEMY_DATABASE_URI = "postgresql://z:123@localhost/mydb"
 SQLALCHEMY_ECHO = True
+BOOTSTRAP_SERVE_LOCAL = True  # bootstrap从本地而非CDN加载js和css文件
